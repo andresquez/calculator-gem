ae7957188eba206c25fe0faf07bd1d592d6a52dd
Andres Eduardo Quezada Bendaña <andresequez@gmail.com>
2023-07-10 11:08:18 -0600

    feat: Add basic calculator functionality
    Inside the Calculator module added the BasicOperations class with the add, subtract, multiply and divide functions

--- calculator/lib/calculator.rb
+++ calculator/lib/calculator.rb
@@ -2,7 +2,36 @@
 
 require_relative "calculator/version"
 
+# Calculator module
 module Calculator
+
+  # Error class
   class Error < StandardError; end
-  # Your code goes here...
+  
+  # Basic Arithmetic Operations Class
+  class BasicOperations
+
+    # Add function, receives two numbers and returns the sum
+    def add(num1, num2)
+      num1 + num2
+    end
+    
+    # Subtract function, receives two numbers and returns the difference
+    def subtract(num1, num2)
+      num1 - num2
+    end
+  
+    # Multiply function, receives two numbers and returns the product
+    def multiply(num1, num2)
+      num1 * num2
+    end
+
+    # Divide function, receives two numbers and returns the quotient
+    def divide(num1, num2)
+      if num2 == 0
+        raise ZeroDivisionError, "Cannot divide by zero"
+      end
+      num1 / num2
+    end
+  
 end
